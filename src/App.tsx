55561850f6109025c3b7eda67937c841868b26ab
Tung Nguyen <38834069+tungnguyen13@users.noreply.github.com>
2023-04-21 21:41:42 -0700

    Deploy React app to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Helmet} from "react-helmet";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+      <div className="container2">
+        <div className="content">
+          <canvas id="universe"></canvas>
+        </div>
+      </div>
+      <div className="title">
+        <h3 className="STARDUST1">From 2015 with Love</h3>
+        <h1 className="STARDUST2">Melk <strong>❤</strong> 幽昙</h1>
+        <h3 className="STARDUST3">Yêu Quỳnh ^^</h3>
+        <img className="img" src="/images/41a9918513bab13320abc80835c0e8bf.JPG" alt="Q"/>
+        <canvas id="pinkboard"></canvas>
+      </div>
+      </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('main') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+
   </React.StrictMode>
 );
 
